cc9a60944b13eed1cd24c2117fc6571bfe8326ba
Marc Toussaint <toussaint@tu-berlin.de>
2022-03-02 18:21:32 +0100

    rendered from database using liquid

---
+++ html/render.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.7
+
+import csv
+from liquid import Environment
+from liquid import FileSystemLoader
+
+# load members
+members_file = csv.DictReader(open("../members.csv"))
+
+members = []
+for row in members_file:
+    members.append(row)
+    
+# render using liquid
+
+env = Environment(loader=FileSystemLoader("templates/"))
+
+template = env.get_template("index.html")
+
+with open('out.html', 'w') as f:
+    f.write(template.render(
+        heading="Some List",
+        page_title="Awesome Title",
+        people=members,
+    ))
